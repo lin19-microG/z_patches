b3d5f4b6774ec107575e5a319fc39538075cf699
MSe <mse1969@posteo.de>
2022-12-02 20:56:29 +0100

    z_patches: Initial commit lin-19.1-microG Branch

---
+++ patches_reverse.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+clear_set() {
+  cd $1
+  echo "Clearing: $PWD"
+  git add . > /dev/null
+  git stash > /dev/null
+  find -name *.orig | while read LINE; do rm $LINE; done
+  find -name *.rej | while read LINE; do rm $LINE; done
+  git clean -f > /dev/null
+  git stash clear > /dev/null
+  git stash > /dev/null
+  cd $TOPDIR
+}
+
+THISDIR=$PWD
+
+cd ..
+TOPDIR=$PWD
+
+
+clear_set art
+clear_set external/conscrypt
+clear_set frameworks/ex
+clear_set frameworks/opt/net/wifi
+clear_set packages/apps/Bluetooth
+clear_set packages/apps/Nfc
+clear_set packages/apps/SetupWizard
+clear_set packages/providers/DownloadProvider
+clear_set system/bt
+clear_set system/extras
+clear_set system/netd
+
+cd $THISDIR
